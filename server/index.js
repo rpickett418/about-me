913babd1585e7365dcd0bb09155d41e0d4e6eed6
rpickett418 <86981398+rpickett418@users.noreply.github.com>
2021-08-12 14:12:10 -0600

    about-me

---
+++ server/index.js
@@ -0,0 +1,25 @@
+// const express = require('express');
+// const path = require('path');
+
+// const app = express()
+// const port = process.env.PORT || 4444
+// app.use(express.static('client'))
+
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, '../client/index.html'));
+// });
+
+
+
+// app.listen(port, () => {
+//     console.log(`Listening on port ${port}`)
+// })
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 5050
+
+app.use(express.static('client'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../client/index.html')))
+
+app.listen(port, () => console.log(`We cruisin now boyyyysss, we out here on ${port}!!`))
