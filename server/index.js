39bf8a17691cbf0c2f6ede3c9ead2fdc3c923aad
rpickett418 <86981398+rpickett418@users.noreply.github.com>
2021-08-12 14:21:12 -0600

    about-pizza

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 5050
 
-app.use(express.static('client'));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../client/index.html')))
+app.use(express.static('pizza'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../pizza/index.html')))
 
 app.listen(port, () => console.log(`We cruisin now boyyyysss, we out here on ${port}!!`))
